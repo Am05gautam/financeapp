f81f9aea2d94bba5ce210d2cbeb670f90de3d79b
Am05gautam <am05gautam@gmail.com>
2020-06-11 20:36:03 +0530

    environment.firebase-error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyASMMaU_nrgV9k-s2PjeuJ8Rd5GJSQk-Rs",
+    authDomain: "finance--app.firebaseapp.com",
+    databaseURL: "https://finance--app.firebaseio.com",
+    projectId: "finance--app",
+    storageBucket: "finance--app.appspot.com",
+    messagingSenderId: "400038784741",
+    appId: "1:400038784741:web:1970f67228a053e2db6e0c",
+    measurementId: "G-PJE8KSRQVQ"
+  }
 };
